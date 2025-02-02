2c5999b85e4bc40bfeb09d1b6fce0fdacea3104f
harshit-godhani <godhaniharshit871@gmail.com>
2025-02-02 12:06:51 +0530

    docker changes

--- src/config.py
+++ src/config.py
@@ -4,5 +4,5 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 60
 REFRESH_TOKEN_EXPIRE_MINUTES = 60
 
 DATABASE_URL = "postgresql://postgres:123456@host.docker.internal:5432/test1"
-
+# DATABASE_URL = "postgresql://postgres:123456@localhost:5432/test1"
 

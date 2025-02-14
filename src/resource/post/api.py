3fb7b2e8c37bab7fd8873da7e11e0b7d8fef80f1
harshit-godhani <godhaniharshit871@gmail.com>
2025-02-14 14:09:21 +0530

    add websockt code in post

--- src/resource/post/api.py
+++ src/resource/post/api.py
@@ -1,12 +1,14 @@
-from fastapi import APIRouter, Depends, File, Form, HTTPException, Security, UploadFile
+from fastapi import APIRouter, Depends, File, Form, HTTPException, Security, UploadFile,WebSocket,WebSocketDisconnect
 from src.resource.post.schema import PostLikeSchema, PostSchema,PostUpdateSchema
 from sqlalchemy.orm import Session
 from database import get_db
 from src.functionality.post.post import create_post,post_update,read_all_post,delete_post
 from src.functionality.post.postlike import post_like
 from src.utils.utils import security
+# import json
 
 
+# active_connections = [] 
 post_router = APIRouter(tags=["User-Post"])
 
 @post_router.post("/create-post/")
@@ -22,6 +24,35 @@ def create_user_post(user_id: int = Form(...),
         return post
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+# @post_router.websocket("/ws/create-post/")
+# async def websocket_create_post(websocket: WebSocket):
+#     await websocket.accept()
+#     active_connections.append(websocket)
+
+#     try:
+#         while True:
+#             data = await websocket.receive_text()
+#             post_data = json.loads(data)
+
+#             user_id = post_data["user_id"]
+#             title = post_data["title"]
+#             content = post_data["content"]
+#             image_filename = post_data["image_filename"]
+            
+#             db: Session = next(get_db())
+#             post_schema = PostSchema(user_id=user_id, title=title, content=content)
+#             post = create_post(post=post_schema, db=db, image=image_filename)  
+
+#             for conn in active_connections:
+#                 await conn.send_json(post)
+
+#     except WebSocketDisconnect:
+#         active_connections.remove(websocket)
+#     except Exception as e:
+#         await websocket.send_json({"error": str(e)})
+
 
 @post_router.patch("/update-post/")
 def user_post_update(post:PostUpdateSchema,db:Session=Depends(get_db)):

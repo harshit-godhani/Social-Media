3dc56504406899b3988704f36f3dfd7bf5ef6896
harshit-godhani <godhaniharshit871@gmail.com>
2025-02-20 15:49:26 +0530

    config file changes

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     ALGORITHM = os.getenv("ALGORITHM")
-    ACCESS_TOKEN = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-    REFRESH_TOKEN = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES"))
+    ACCESS_TOKEN = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+    REFRESH_TOKEN = os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES")
     DB_URL = os.getenv("DATABASE_URL")
 

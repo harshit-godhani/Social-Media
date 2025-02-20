e0490476d96443a5b91f4e1ec27aee4555139065
harshit-godhani <godhaniharshit871@gmail.com>
2025-02-20 15:14:26 +0530

    req.txt file changes

--- src/functionality/follower/followers.py
+++ src/functionality/follower/followers.py
@@ -6,23 +6,26 @@ from src.resource.follower.schema import FollowerSchema,UnfollowSchema,Allfollow
 from database import get_db
 from datetime import datetime
 
-def user_follower(follower:FollowerSchema,db:Session=Depends(get_db)):
 
+def user_follower(follower:FollowerSchema,db:Session=Depends(get_db)):
     user = db.query(UserModel).filter(UserModel.id == follower.user_id).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     follow_data = db.query(UserModel).filter(UserModel.id == follower.follower_id).first()
     if not follow_data:
         raise HTTPException(status_code=404,detail="follower not found!")
+    
     db_follower=UserFollowerModel(
         user_id=follower.user_id,
         follower_id=follower.follower_id,
         created_at=datetime.utcnow()
     )
-    db.add(db_follower)
     user.follower_count = (user.follower_count or 0) +1
+
+    db.add(db_follower)
     db.commit()
     db.refresh(db_follower)
+
     return {
         "success":True,
         "message":f"you successfully followed user whoses user_id is {follower.follower_id}."
@@ -35,6 +38,7 @@ def user_unfollower(unfollow_user:UnfollowSchema,db:Session=Depends(get_db)):
     unfollow_data = db.query(UserFollowerModel).filter(UserFollowerModel.follower_id == unfollow_user.follower_id).first()
     if not unfollow_data:
         raise HTTPException(status_code=404,detail="follower not found!")
+        
     db.delete(unfollow_data)
     db.commit()
 

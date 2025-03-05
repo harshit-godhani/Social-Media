124593b5897de84aa6e414f504cf3906943f2205
harshit-godhani <godhaniharshit871@gmail.com>
2025-03-05 15:25:57 +0530

    copy function remove

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -15,16 +15,6 @@ refresh_token = Config.REFRESH_TOKEN
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def verify_token(token: str):
-    secret_key = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-    algorithms = ["HS256"]
-    try:
-        payload = jwt.decode(token, secret_key, algorithms=algorithms)
-        return payload
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid or expired token")
-    
-
 def send_email(to_email, subject, body):
     smtp_server = "smtp.gmail.com"
     smtp_port = 587

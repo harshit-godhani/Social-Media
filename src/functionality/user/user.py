5b36e07b30e4afaca645b7d40332bf72504cc719
harshit-godhani <godhaniharshit871@gmail.com>
2025-03-06 16:31:25 +0530

    user.py

--- src/functionality/user/user.py
+++ src/functionality/user/user.py
@@ -66,9 +66,11 @@ def user_login(user:UserLoginSchema,db : Session =Depends(get_db)):
 
     return {
         "Message":"Login Successfully",
+        "user_id":db_user.id,
         "access_token": access_token,
         "refresh_token": refresh_token,
         "token_type": "bearer"
+
     }
 
   
